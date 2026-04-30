db32a88894ce5261d81c41f77de395c9f7c3e998
orzion <skhn7412@gmail.com>
2026-04-30 18:02:45 +0300

    constructer function for customer

--- ConsoleApp26/Customer.cs
+++ ConsoleApp26/Customer.cs
@@ -8,6 +8,13 @@ namespace ConsoleApp26
         private string name;
         private string phoneNumber;
 
+        public Customer(string id, string name, string phoneNumber)
+        {
+            this.id = id;
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
         public void PrintDetails()
         {
             Console.WriteLine($"ID: {id}, Name: {name}, Phone: {phoneNumber}");
